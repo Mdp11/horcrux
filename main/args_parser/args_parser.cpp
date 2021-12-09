89694334592c549f2540b05f6472c397fab5c1ff
mdp11 <mattia.dipi@gmail.com>
2021-12-09 20:40:21 +0100

    Update usage

--- main/args_parser/args_parser.cpp
+++ main/args_parser/args_parser.cpp
@@ -13,5 +13,10 @@ args_parsing::ArgsParser::ArgsParser(int argc, char **argv)
 
 void args_parsing::ArgsParser::printUsage()
 {
-	std::cout << "USAGE" << std::endl;
+	std::cout << "### Horcrux usage ###" << std::endl << std::endl;
+	std::cout << "To split your file into horcruxes use the following command:" << std::endl;
+	std::cout << "horcrux create -n <number_of_horcruxes> <input_file> <output_folder>" << std::endl;
+	std::cout << std::endl;
+	std::cout << "To merge horcruxes back into your file use the following command:" << std::endl;
+	std::cout << "horcrux load -k <decryption_key> <input_file_1> ... <input_file_n> <output_file>" << std::endl;
 }
